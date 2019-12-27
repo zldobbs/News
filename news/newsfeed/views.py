fe4bbd9a2a644256b77bc4d378aa93ec0237e9ae
Zach Dobbs <dobbszach@gmail.com>
2019-12-26 23:34:34 -0600

    A little more customization on the API

--- news/newsfeed/views.py
+++ news/newsfeed/views.py
@@ -7,7 +7,12 @@ NEWS_KEY = '5fc766119a1e4ee8a20567bece674814'
 
 # Create your views here.
 def index(request): 
-    url = ('https://newsapi.org/v2/top-headlines?country=us&apiKey=' + NEWS_KEY)
+    url = ( 
+        'https://newsapi.org/v2/top-headlines?'
+        'country=us&'
+        'category=business&'
+        'apiKey=' + NEWS_KEY
+    )
     response = requests.get(url)
     data = response.json()
     context = { 'feed': data['articles'] }
